0d04303f55b1b9f094236caa00e6bc0056a35072
CauveryRaja <cauveryraja25@gmail.com>
2023-04-13 12:57:00 +0530

    Add Water allotment form

--- src/components/Allot/Allot.jsx
+++ src/components/Allot/Allot.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react"
+
 const Allot = () => {
-    return <div>Allot water</div>
+    const [allotmentInfo, setAllotmentInfo] = useState({});
+
+    const changeListener = (event) => {
+        if(event.target.value) {
+            setAllotmentInfo({...allotmentInfo, [event.target.id]: event.target.value})
+        }
+    }
+
+    useEffect(() => console.log(allotmentInfo), [allotmentInfo])
+
+    return <div>
+        <h2>Allot water</h2>
+        <form>
+            <div>
+                <label htmlFor="apartmentType">Apartment Type</label>
+                <select name="apartmentType" id="apartmentType" value={allotmentInfo.apartmentType} onChange={changeListener}>
+                    <option value="3">2 BHK</option>
+                    <option value="5">3 BHK</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="corporationWater">Corporation Water</label>
+                <input type="number" name="corporationWater" id="corporationWater" value={allotmentInfo.corporationWater} onChange={changeListener}/>
+                :
+                <label htmlFor="boreWellWater">Borewell Water</label>
+                <input type="number" name="boreWellWater" id="boreWellWater" value={allotmentInfo.boreWellWater} onChange={changeListener}/>
+            </div>
+        </form>
+    </div>
 }
 
 export default Allot
